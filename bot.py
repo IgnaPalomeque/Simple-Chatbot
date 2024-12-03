d0b1575739614e3190739c7aca2773fab8938783
IgnaPalomequee <nachopalomeque5@gmail.com>
2024-12-03 17:50:38 -0300

    feat(clean-text): add functions to clean given text
    Added clean_text function to clean the text before tokenizing it. Also separated the text in two different arrays input and target.
    
    feat(tokenize-text): tokenize text
    
    Tokenize both input and target arrays. Also added padding to target the same lenght in every sequence.

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import re
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Input
+from tensorflow.keras.models import Model
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
@@ -42,3 +44,29 @@ for conv in conversations:
         input_text = conv[i]
         target_text = conv[i + 1]
         pairs.append((input_text, target_text))
+
+# Clean text given
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z?.!,]+", " ", text)
+    text = re.sub(r"[.]+", ".", text)
+    return text
+
+# Sets different arrays for questions and answers and cleans them
+input_texts = [clean_text(pair[0]) for pair in pairs]
+target_texts = ["<start> " + clean_text(pair[1]) + " <end>" for pair in pairs]
+
+# Creates a dicctionary of word with its own number
+tokenizer = Tokenizer(filters='')
+tokenizer.fit_on_texts(input_texts + target_texts)
+
+# Replaces each word with the predefined number
+input_sequences = tokenizer.texts_to_sequences(input_texts)
+target_sequences = tokenizer.texts_to_sequences(target_texts)
+
+# Adds padding to target the max sequence length
+max_seq_length = max([len(seq) for seq in input_sequences + target_sequences])
+input_sequences = pad_sequences(input_sequences, maxlen=max_seq_length, padding='post')
+target_sequences = pad_sequences(target_sequences, maxlen=max_seq_length, padding='post')
+
+vocab_size = len(tokenizer.word_index) + 1

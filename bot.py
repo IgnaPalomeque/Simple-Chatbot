35f43ed4c385d1d0d0dd51abb9ae8abc03119cd0
IgnaPalomequee <nachopalomeque5@gmail.com>
2024-12-05 16:38:20 -0300

    feat(train-model) add train model function
            added the function to train a model if train_model is set to true, by default is set to false.
    
    feat(model-parameters) add variables for model parameters
    
            added variables to change the model parameters such as the amount of data to use to train the model or the amount of filter for each layer. Parameters are set to utilize a very small amount of data by default as well as to not use too many filters for the layers since i had to train a model fast for future testing.

--- bot.py
+++ bot.py
@@ -2,11 +2,22 @@ import numpy as np
 import pandas as pd
 import re
 import tensorflow as tf
+import random
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Input
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Input, GRU
 from tensorflow.keras.models import Model
 from pathlib import Path
+
+# Model parameters
+pairs_fraction = 0.10 # 1 to use 100% of the data
+tokenizer_words = 25000 # Maximum number of unique words
+max_seq_length = 50 # Maximum length for a sequence
+embedding_filters = 128 # Amount of filters for the embedding layer
+lstm_filters = 128 # Amount of filters for the lstm layer
+model_batch_size = 16 # Batch size to feed the model
+model_epochs = 10 # Amount of epochs
+train_model = False # Set to true to train a new model. By default uses one already trained 
 
 ROOT_DIR = Path(__file__).parent
 
@@ -52,12 +63,14 @@ def clean_text(text):
     text = re.sub(r"[.]+", ".", text)
     return text
 
+sampled_pairs = random.sample(pairs, int(len(pairs) * pairs_fraction))
+
 # Sets different arrays for questions and answers and cleans them
-input_texts = [clean_text(pair[0]) for pair in pairs]
-target_texts = ["<start> " + clean_text(pair[1]) + " <end>" for pair in pairs]
+input_texts = [clean_text(pair[0]) for pair in sampled_pairs]
+target_texts = ["<start> " + clean_text(pair[1]) + " <end>" for pair in sampled_pairs]
 
 # Creates a dicctionary of word with its own number
-tokenizer = Tokenizer(filters='')
+tokenizer = Tokenizer(num_words=tokenizer_words,filters='')
 tokenizer.fit_on_texts(input_texts + target_texts)
 
 # Replaces each word with the predefined number
@@ -65,7 +78,9 @@ input_sequences = tokenizer.texts_to_sequences(input_texts)
 target_sequences = tokenizer.texts_to_sequences(target_texts)
 
 # Adds padding to target the max sequence length
-max_seq_length = max([len(seq) for seq in input_sequences + target_sequences])
+
+#max_seq_length = max([len(seq) for seq in input_sequences + target_sequences])//2
+
 input_sequences = pad_sequences(input_sequences, maxlen=max_seq_length, padding='post')
 target_sequences = pad_sequences(target_sequences, maxlen=max_seq_length, padding='post')
 
@@ -73,18 +88,42 @@ vocab_size = len(tokenizer.word_index) + 1
 
 # Definition of the encoder
 encodoer_inputs = Input(shape=(max_seq_length,))
-encoder_embedding = Embedding(vocab_size, 256)(encodoer_inputs)
-encoder_lstm, state_h, state_c, = LSTM(256, return_state=True)(encoder_embedding)
-encoder_states = [state_h,state_c]
+encoder_embedding = Embedding(vocab_size, embedding_filters)(encodoer_inputs)
+encoder_gru, state_h, state_c = LSTM(lstm_filters, return_state=True)(encoder_embedding)
+encoder_states = [state_h, state_c]
 
 # Definition of the decoder
 decoder_inputs = Input(shape=(max_seq_length,))
-decoder_embedding = Embedding(vocab_size, 256)(decoder_inputs)
-decoder_lstm = LSTM(256, return_sequences=True, return_state=False)
-decoder_outputs = decoder_lstm(decoder_embedding, initial_state=encoder_states)
+decoder_embedding = Embedding(vocab_size, embedding_filters)(decoder_inputs)
+decoder_gru = LSTM(lstm_filters, return_sequences=True, return_state=False)
+decoder_outputs = decoder_gru(decoder_embedding, initial_state=encoder_states)
 decoder_dense = Dense(vocab_size, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
+# Creates the model
 model = Model([encodoer_inputs, decoder_inputs], decoder_outputs)
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy')
 model.summary()
+
+# Shift the target sequence to create decoder outputs
+decoder_output_sequences = np.zeros_like(target_sequences)
+decoder_output_sequences[:,:-1] = target_sequences[:, 1:]
+
+# Reshape the data before training
+decoder_output_sequences = decoder_output_sequences[..., np.newaxis]
+
+# Train the model
+if train_model:
+    model.fit(
+        [input_sequences, target_sequences],
+        decoder_output_sequences,
+        batch_size = model_batch_size,
+        epochs = model_epochs,
+        validation_split = 0.2
+    )
+    save_path = ROOT_DIR / "Pretrained models/model.keras"
+    model.save(save_path)
+else:
+    print('------------------------------')
+    print('Skipped training a new model!.')
+    print('------------------------------')
